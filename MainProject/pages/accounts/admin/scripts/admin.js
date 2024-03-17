1383a65b884cf5d44e4ab2d5eb87cf2e6d8a434d
TheBigKei <TheBigKei@yandex.ru>
2024-03-17 13:11:20 +0300

    createPage

---
+++ MainProject/pages/accounts/admin/scripts/admin.js
@@ -0,0 +1,7 @@
+function toCreate(){
+    window.location.href = "/MainProject/pages/accounts/admin/createPage/create.html"
+}
+
+function toUpdate(){
+    window.location.href = "/MainProject/pages/accounts/admin/updatePage/update.html"
+}
