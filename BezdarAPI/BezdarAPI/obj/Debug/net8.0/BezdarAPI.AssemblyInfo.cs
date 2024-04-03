c71c0bd99fcb58d54b9c52094d03d434d14a5b18
TheBigKei <TheBigKei@yandex.ru>
2024-04-03 10:53:30 +0300

    Final commit 2

--- BezdarAPI/BezdarAPI/obj/Debug/net8.0/BezdarAPI.AssemblyInfo.cs
+++ BezdarAPI/BezdarAPI/obj/Debug/net8.0/BezdarAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BezdarAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70e9366a408b459d328aff44847c7da126ecd4bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b9fe82641630eff561840e486ce289c7ca7cbf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BezdarAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BezdarAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
